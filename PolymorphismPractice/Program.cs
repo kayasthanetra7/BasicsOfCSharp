4a56f10a3010d5a2d14e25c0431cecb52269f846
Netra Kayastha <kayastha.netra7@gmail.com>
2020-10-12 15:44:38 -0400

    Adding another class which is an example of Dynamic Polymorphism

--- PolymorphismPractice/Program.cs
+++ PolymorphismPractice/Program.cs
@@ -6,11 +6,21 @@ namespace PolymorphismPractice
     {
         static void Main(string[] args)
         {
-            Calculator c1 = new Calculator(); // creating an instance of the class Calculator
+            Calculator c1 = new Calculator(); // creating an instance of the Calculator class
             c1.AddNumbers(2, 10);
             c1.AddNumbers(2, 100, 12);
-
             Console.WriteLine();
+
+            Shapes circle1 = new Circle();
+            Console.WriteLine("Area of the circle: {0}", circle1.Area());
+
+            Shapes square1 = new Square();
+            Console.WriteLine("Area of the square: {0}", square1.Area());
+
+            Shapes triangle1 = new Triangle();
+            Console.WriteLine("Area of the triangle: {0}", triangle1.Area());
+
+
         }
     }
 }
---
+++ PolymorphismPractice/Shapes.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolymorphismPractice
+{
+    public class Shapes
+    {
+        public virtual double Area()
+        {
+            return 0;
+        }
+    }
+
+    public class Circle : Shapes
+    {
+        public double Radius { get; set; }
+        public Circle()
+        {
+            Radius = 5;
+        }
+        public override double Area()
+        {
+            return 3.14 * Radius * Radius;
+        }
+    }
+
+    public class Square : Shapes
+    { 
+        public double Side { get; set; }
+        public Square()
+        {
+            Side = 4;
+        }
+        public override double Area()
+        {
+            return Side * Side;
+        }
+    }
+
+    public class Triangle : Shapes
+    {
+        public double Base { get; set; }
+        public double Height { get; set; }
+
+        public Triangle()
+        {
+            Base = 6;
+            Height = 12;
+        }
+        public override double Area()
+        {
+            return 0.5 * Base * Height;
+        }
+    }
+}
