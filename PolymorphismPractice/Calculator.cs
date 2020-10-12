6d68aa6b25f85ac51e7e4a269718b9f345f7ff2e
Netra Kayastha <kayastha.netra7@gmail.com>
2020-10-12 14:47:38 -0400

    Adding program for Calculator Class to Add numbers

--- PolymorphismPractice/Calculator.cs
+++ PolymorphismPractice/Calculator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿// This is example of Static Polymorphism, which is based on the concept of method overloading.
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,14 @@ namespace PolymorphismPractice
 {
     public class Calculator
     {
+        public void AddNumbers(int a, int b) // Method AddNumbers takes 2 parameters.
+        {
+            Console.WriteLine("a + b = {0}", a + b);
+        }
+
+        public void AddNumbers(int a, int b, int c) // Method AddNumbers takes 3 parameters.
+        {
+            Console.WriteLine("a + b + c = {0}", a + b + c);
+        }
     }
 }
--- PolymorphismPractice/Program.cs
+++ PolymorphismPractice/Program.cs
@@ -6,7 +6,11 @@ namespace PolymorphismPractice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Calculator c1 = new Calculator(); // creating an instance of the class Calculator
+            c1.AddNumbers(2, 10);
+            c1.AddNumbers(2, 100, 12);
+
+            Console.WriteLine();
         }
     }
 }
